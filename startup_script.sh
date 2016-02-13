f5f294df9acbd9a7b5be4c31ca272743122f1f66
fishkingsin <fishkingsin@gmail.com>
2016-02-14 02:38:13 +0800

    udpate

--- startup_script.sh
+++ startup_script.sh
@@ -33,11 +33,11 @@ start mosquitto deamon
 mosquitto -d
 
 cd /home/pi/py-beacon
-start beacon collactor and emitter
+echo "start beacon collactor and emitter"
 sudo python ./collector.py &
 sudo python ./emitter.py &
 
-gotto working directory
+echo "gotto working directory"
 cd /home/pi/CollarPlayground
 sudo python select_language.py
 echo 'start the game'
