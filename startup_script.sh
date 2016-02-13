43af513c3db033214fc854ef3ee4a3028643f893
fishkingsin <fishkingsin@gmail.com>
2016-02-13 19:22:40 +0800

    update startup script

--- startup_script.sh
+++ startup_script.sh
@@ -1,12 +1,43 @@
-#start mosquitto deamon 
-# mosquitto -d
+#/bin/sh
+#enable gpio 16 buzzer
+gpio mode 16 out
+#fire
+max=3
+for i in `seq 1 $max`
+do  
+	echo $i
+	sleep 1
+	gpio -g write 16 1
+	sleep 1
+	gpio -g write 16 0
+	sleep 1
+done
+sleep 1
+#startup jingle
+mpg321 /home/pi/CollarPlayground/Mac_Startup_Sound.mp3 &
+#mount disk if avaiable
+echo "mount /media/usb"
+find /dev/  -name "sda1" -print0 | xargs -0 -r -I file mount file /media/usb
+sleep 2
+for i in /media/usb/contents/* 
+do
+	echo "cp \"$i\" /home/pi/CollarPlayground/"
+	cp "$i" /home/pi/CollarPlayground/
+done
+sleep 2
+echo "unmount /media/usb"
+sudo umount /media/usb
+echo "done"
+#start core process
+start mosquitto deamon 
+mosquitto -d
 
-# cd /home/pi/py-beacon
-#start beacon collactor and emitter
-# sudo python ./collector.py &
-# sudo python ./emitter.py &
+cd /home/pi/py-beacon
+start beacon collactor and emitter
+sudo python ./collector.py &
+sudo python ./emitter.py &
 
-#gotto working directory
+gotto working directory
 cd /home/pi/CollarPlayground
 sudo python select_language.py
 echo 'start the game'
