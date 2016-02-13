5f5d265851ea1eb4bacb84bfc05f598fab200e99
fishkingsin <fishkingsin@gmail.com>
2016-02-13 23:43:22 +0800

    uddate dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install rcconf mpg321 python-pip mosquitto install python-smbus bluez python-bluez python-numpy libc-ares-dev raspi-gpio
+sudp pip install paho-mqtt
 sudo pip install rx
 sudo cp ./rc.local /etc/rc.local
