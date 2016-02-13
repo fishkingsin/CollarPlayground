8a95cc1de9ba238d09baf392f36ea001c4d0caa6
fishkingsin <fishkingsin@gmail.com>
2016-02-13 23:39:50 +0800

    update install dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install mpg321 python-pip mosquitto
+sudo apt-get install rcconf mpg321 python-pip mosquitto install python-smbus bluez python-bluez python-numpy libc-ares-dev raspi-gpio
 sudo pip install rx
 sudo cp ./rc.local /etc/rc.local
