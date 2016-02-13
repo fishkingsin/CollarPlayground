8f467edd799cde72bd26399bc9f4de0d96c3eef1
fishkingsin <fishkingsin@gmail.com>
2016-02-13 23:31:26 +0800

    upadte

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install mpg321 python-pip python-pygame 
+sudo apt-get install mpg321 python-pip mosquitto
 sudo pip install rx
+sudo cp ./rc.local /etc/rc.local
