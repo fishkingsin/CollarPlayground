e7a49ff86ad91794c2573a37503ade87a4e7ed96
fishkingsin <fishkingsin@gmail.com>
2016-02-14 00:41:51 +0800

    update script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 sudo apt-get upgrade
-sudo apt-get install rcconf mpg321 python-pip mosquitto install python-smbus bluez python-bluez python-numpy libc-ares-dev raspi-gpio
+sudo apt-get install rcconf mpg321 python-pip mosquitto python-smbus bluez python-bluez python-numpy libc-ares-dev raspi-gpio
 sudo pip install paho-mqtt
 sudo pip install rx
 sudo cp ./rc.local /etc/rc.local
